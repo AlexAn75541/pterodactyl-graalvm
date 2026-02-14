d78647648c5aade0841242251d7518dc1b8d06ca
Aretzera <agroup168@gmail.com>
2026-02-14 19:36:08 +0700

    rough addition of both mallocs from Skullian's

--- scripts/entrypoint_exp.sh
+++ scripts/entrypoint_exp.sh
@@ -50,9 +50,10 @@ export PATH="${JAVA_HOME}/bin:${PATH}"
 cd /home/container || exit 1
 
 # Some color shit
-
 LIGHT_BLUE='\033[1;34m'
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+LIGHT_RED='\033[1;31m' 
 RESET_COLOR='\033[0m'
 
 # Print Java version
@@ -60,13 +61,126 @@ printf "${LIGHT_BLUE}container@java-info~ ${RESET_COLOR}java -version\n"
 java -version 2>&1 | cat
 echo ""
 
+
+# initial lines for implementing Skullians's stuff(imma copy all of em) 
+
+
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+DUMPS_ENABLED=$(echo "$PARSED" | sed -n 's/.*-Ddump=\([^ ]*\).*/\1/p')
+TRACE_ENABLED=$(echo "$PARSED" | sed -n 's/.*-Danalyse=\([^ ]*\).*/\1/p')
+JEMALLOC_DISABLED=$(echo "$PARSED" | sed -n 's/.*-Djemalloc=false.*/true/p')
+# combined all in a img, so imma make an error handling for this, its either each or nothing of them
+MIMALLOC_DISABLED=$(echo "$PARSED" | sed -n 's/.*-Dmimalloc=false.*/true/p')
+
+if [ -z "$JEMALLOC_DISABLED" ] && [ -z "$MIMALLOC_DISABLED" ]; then
+    printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}${LIGHT_RED}ERROR: Both jemalloc and mimalloc are enabled!${RESET_COLOR}\n"
+    printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}You must disable either one of them\n"
+    printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}or just disable them all for your sake! \n"
+    exit 1
+fi
+
+if [ -z "$JEMALLOC_DISABLED" ]; then
+    printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}Enabling jemalloc support\n"
+    export LD_PRELOAD="/usr/local/lib/libjemalloc.so"
+fi
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}%s\n" "$PARSED"
+
+# failsafe in case dumps folder does not exist
+mkdir -p dumps
+
+
+
+
+# haha we hate nohup
+if [ "$DUMPS_ENABLED" = "true" ]; then
+    export MALLOC_CONF="prof:true,lg_prof_interval:31,lg_prof_sample:17,prof_prefix:/home/container/dumps/jeprof,background_thread:true,dirty_decay_ms:1000,muzzy_decay_ms:0,narenas:1,tcache_max:1024,abort_conf:true"
+
+    (
+        while true; do
+            # loop through heapdump files
+            for heapfile in dumps/*.heap; do
+                if [ -f "$heapfile" ]; then
+                    basefilename="${heapfile%.heap}"
+                    
+                    timestamp=$(date +"%d.%m.%y-%H:%M:%S")
+                    
+                    gif_output="dumps/output/${basefilename}-${timestamp}.gif"
+                    
+                    mkdir -p "$(dirname "$gif_output")"
+                    
+                    jeprof --show_bytes --maxdegree=20 --nodefraction=0 --edgefraction=0 --gif \
+                        "${JAVA_HOME}/bin/java" \
+                        "$heapfile" > "$gif_output"
+                    
+                    # Remove processed heap file
+                    rm "$heapfile"
+                fi
+            done
+            
+            # Wait one minute before checking again
+            sleep 60
+        done
+    ) &
+fi
+
+if [ "$TRACE_ENABLED" = "true" ]; then
+    # Extract the keyword from the PARSED variable
+    KEYWORD=$(echo "$PARSED" | sed -n 's/.*-Dkeyword=\([^ ]*\).*/\1/p')
+    INTERVAL=$(echo "$PARSED" | sed -n 's/.*-Dinterval=\([^ ]*\).*/\1/p')
+
+    if [ -z "$KEYWORD" ]; then
+        printf "KEYWORD is empty. Ensure -Dkeyword is set.\n"
+        exit 1
+    fi
+    if [ -z "$INTERVAL" ]; then
+        printf "INTERVAL is empty. Ensure -Dinterval is set. (In seconds)\n"
+        exit 1
+    fi
+
+    printf "Searching for keyword $KEYWORD\n"
+
+    (
+        mkdir -p dumps/traces
+
+        while true; do
+            sleep "$INTERVAL"
+
+            PID=$(pgrep java)
+            jstack "${PID}" > "profiling.log"
+
+            JVM_LOG="profiling.log"
+
+            if [ -f "$JVM_LOG" ]; then
+                timestamp=$(date +"%d.%m.%y-%H:%M:%S")
+                TRACE_OUTPUT="dumps/traces/trace-${timestamp}.log"
+
+                if grep -qE "$KEYWORD" "$JVM_LOG"; then
+                    cat "$JVM_LOG" > "$TRACE_OUTPUT"
+
+                    printf "Detected keyword (%s):" "$KEYWORD" >> "$TRACE_OUTPUT"
+                    grep -E "$KEYWORD" "$JVM_LOG" >> "$TRACE_OUTPUT"
+                fi
+            fi
+        done
+    ) &
+fi
+
+
+# all ts for mimmaloc is something
+MIMALLOC_DISABLED=$(echo "$PARSED" | sed -n 's/.*-Dmimalloc=false.*/true/p')
+if [ -z "$MIMALLOC_DISABLED" ]; then
+    printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}Enabling mimalloc support\n"
+    export LD_PRELOAD="/usr/local/lib/libmimalloc.so"
+fi
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
 printf "${GREEN}container@game-panel-command~ ${RESET_COLOR}%s\n" "$PARSED"
+
 # shellcheck disable=SC2086
-eval ${PARSED}
+exec env ${PARSED}
